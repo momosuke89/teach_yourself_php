f96fe78e3c340bda0f518c769091cf3ebb181cb9
momosuke89 <momota.masashi@gmail.com>
2014-07-22 14:00:50 +0900

    delete past files

--- 10_1/area_index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'Area.php';
-
-//静的メソッドの呼び出し
-print '四角形の面積: ' . Area::square(10, 20) . 'cm^2';
-
-?>
--- 10_1/person_index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'Person.php';
-
-$p = new Person('凛生', '山田');
-$p->show();
-
-?>
